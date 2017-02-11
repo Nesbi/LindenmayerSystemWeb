5d4d5266c67c967716936d8d4380f0da749a6290
Nico Balbach <nico.balbach@stud-mail.uni-wuerzburg.de>
2017-02-11 01:18:12 +0100

    calculating with angles now. first perfect tree

--- js/lviewer.js
+++ js/lviewer.js
@@ -9,85 +9,31 @@ window.onload = function() {
   var tree = ldemo.tree;
   var start = new paper.Point(400,500);
 
-  drawTree(tree, start, 60);
-
-  /*var path = new paper.Path();
-  path.strokeColor = 'green';
-
-  // Move to start and draw a line from there
-	path.moveTo(start);
-	// Note that the plus operator on Point objects does not work
-	// in JavaScript. Instead, we need to call the add() function:
-	path.lineTo(start.add([ 200, -50 ]));
-  */paper.view.draw();
+  drawTree(tree, start, 100);
+  paper.view.draw();
 }
 
 drawTree = function(tree,startPoint,startLength){
-  drawBranch(tree.getRoot(), startPoint, new paper.Point(0,1), startLength);
+  drawBranch(tree.getRoot(), startPoint, 0, startLength);
 }
 
-drawBranchx = function(node, point, length){
-  if(length > 1){
-    var children = node.getChildren();
-    var childrenLength = children.length;
-    var individualLength = 3*length / childrenLength;
-    var curPoint = new paper.Point(point.x-(individualLength*childrenLength/2),point.y-length);
-    for(var childIndex = 0; childIndex < childrenLength; childIndex++){
-      var child = children[childIndex];
-      //var childPoint = new paper.Point(point);
-      var childPoint = new paper.Point(curPoint.x+individualLength,curPoint.y);
-
-      var path = new paper.Path();
-      switch (child.data) {
-        case 1:
-          path.strokeColor = 'green';
-          break;
-        case 2:
-          path.strokeColor = 'blue';
-          break;
-        case 3:
-          path.strokeColor = 'red';
-          break;
-        case 4:
-          path.strokeColor = 'yellow';
-          break;
-        default:
-          path.strokeColor = 'white';
-      }
-
-      path.add(point);
-      path.lineTo(childPoint);
-
-      curPoint = childPoint;
-
-      drawBranch(child,childPoint,4*length/5);
-      //drawBranch(child,childPoint,4*individualLength/5);
-    }
-  }
-}
-
-drawBranch = function(node, point, vector, length){
+drawBranch = function(node, point, angle, length){
   var children = node.getChildren();
   var childrenLength = children.length;
-  var deltaAngle = 360/(childrenLength+1);
-  var endPoint = new paper.Point(vector);
-  endPoint.length = length;
+  var deltaAngle = 360/(childrenLength);
+  var endPoint = new paper.Point(0,-length);
+  endPoint = endPoint.rotate(angle, new paper.Point(0,0));
   endPoint = endPoint.add(point);
   for(var childIndex = 0; childIndex < childrenLength; childIndex++){
     var child = children[childIndex];
-    var childPoint;
-/*    if(childrenLength === 1){
-      childPoint = endPoint.rotate(180,point);
-    }else{*/
-      childPoint = endPoint.rotate(deltaAngle*(childIndex+1),point);
-    //}
+    var childPoint = endPoint.rotate(deltaAngle*(childIndex),point);
     var path = new paper.Path();
     path.strokeColor = 'white';
 
     path.add(point);
     path.lineTo(childPoint);
 
-    drawBranch(child,childPoint,endPoint.subtract(point),4*length/5);
+    drawBranch(child,childPoint,angle-deltaAngle,length/2);
     //drawBranch(child,childPoint,4*individualLength/5);
   }
 }
