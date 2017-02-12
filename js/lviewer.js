5354aeb555ec62cef13edbc14d9e6d46fc096013
Nico Balbach <nico.balbach@stud-mail.uni-wuerzburg.de>
2017-02-12 01:28:32 +0100

    +Iteration by mouseclick, +shortening factor

--- js/lviewer.js
+++ js/lviewer.js
@@ -3,38 +3,45 @@ window.onload = function() {
   paper.setup(canvas);
 
   var ldemo = demo();
-  for(var i = 0; i < 10; i++){
+  /*for(var i = 0; i < 8; i++){
     ldemo.nextIteration();
-  }
+  }*/
   var tree = ldemo.tree;
   var start = new paper.Point(400,500);
 
-  drawTree(tree, start, 100);
+  drawTree(tree, start, 280);
   paper.view.draw();
+
+  window.onmousedown = function(){
+    ldemo.nextIteration();
+    paper.project.clear();
+    drawTree(tree,start,280);
+};
 }
 
 drawTree = function(tree,startPoint,startLength){
-  drawBranch(tree.getRoot(), startPoint, 0, startLength);
+  drawBranch(tree.getRoot(), startPoint, 0, startLength, 2/5);
 }
 
-drawBranch = function(node, point, angle, length){
-  var children = node.getChildren();
-  var childrenLength = children.length;
-  var deltaAngle = 360/(childrenLength);
-  var endPoint = new paper.Point(0,-length);
-  endPoint = endPoint.rotate(angle, new paper.Point(0,0));
-  endPoint = endPoint.add(point);
-  for(var childIndex = 0; childIndex < childrenLength; childIndex++){
-    var child = children[childIndex];
-    var childPoint = endPoint.rotate(deltaAngle*(childIndex),point);
-    var path = new paper.Path();
-    path.strokeColor = 'white';
+drawBranch = function(node, point, angle, length, shortening){
+  if(length > 1){
+    var children = node.getChildren();
+    var childrenLength = children.length;
+    var deltaAngle = (360/(childrenLength));
+    var endPoint = new paper.Point(0,-length);
+    endPoint = endPoint.rotate(angle, new paper.Point(0,0));
+    endPoint = endPoint.add(point);
+    for(var childIndex = 0; childIndex < childrenLength; childIndex++){
+      var child = children[childIndex];
+      var childPoint = endPoint.rotate(deltaAngle*(childIndex+1),point);
+      var path = new paper.Path();
+      path.strokeColor = 'white';
 
-    path.add(point);
-    path.lineTo(childPoint);
+      path.add(point);
+      path.lineTo(childPoint);
 
-    drawBranch(child,childPoint,angle-deltaAngle,length/2);
-    //drawBranch(child,childPoint,4*individualLength/5);
+      drawBranch(child,childPoint,angle-deltaAngle,length*shortening, shortening);
+    }
   }
 }
 
@@ -44,8 +51,8 @@ demo = function(){
   var axiom = 1;
   var rules = [];
   rules.push(new LRule(1,[2]));
-  rules.push(new LRule(2,[2,3,4]));
-  rules.push(new LRule(3,[2,3,4]));
-  rules.push(new LRule(4,[2,3,4]));
+  rules.push(new LRule(2,[2,2,2,2,2]));
+  rules.push(new LRule(3,[3,2,3]));
+  rules.push(new LRule(4,[2,2,2]));
   return new LSystem(variables, constants, axiom, rules);
 }
